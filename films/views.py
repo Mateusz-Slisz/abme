2b64c585ee6bd776da74582422e63b71c09fe7cf
Mateusz Slisz <mat.slisz@yahoo.com>
2017-11-26 22:31:10 +0100

    A-tag, top rated.

--- films/views.py
+++ films/views.py
@@ -9,7 +9,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 class Round(Func):
     function = 'ROUND'
-    template='%(function)s(%(expressions)s, 1)'
+    template = '%(function)s(%(expressions)s, 1)'
 
 
 def list(request):
@@ -33,7 +33,7 @@ def list(request):
             if del_film_id is not None and rate is not None:
                 film = get_object_or_404(Film, id=del_film_id)
                 FilmRating.objects.filter(user=activ_user, film=film, rate=rate).delete()
-            
+
             if add_watchlist is not None:
                 film = get_object_or_404(Film, id=add_watchlist)
                 if FilmWatchlist.objects.filter(user=activ_user, film=film).exists():
@@ -45,7 +45,8 @@ def list(request):
                 film = get_object_or_404(Film, id=del_watchlist)
                 FilmWatchlist.objects.filter(user=activ_user, film=film).delete()
 
-        film_list = Film.objects.get_queryset().order_by('id').annotate(average_score=Round(Avg('filmrating__rate')))
+        film_list = Film.objects.get_queryset().order_by('id').annotate(
+            average_score=Round(Avg('filmrating__rate')))
 
         filmrating = FilmRating.objects.filter(user=activ_user)
         f_id = filmrating.values_list('film__id', flat=True)
@@ -71,7 +72,8 @@ def list(request):
         }
         return render(request, 'film/list.html', context)
     else:
-        film_list = Film.objects.get_queryset().order_by('id').annotate(average_score=Round(Avg('filmrating__rate')))
+        film_list = Film.objects.get_queryset().order_by('id').annotate(
+            average_score=Round(Avg('filmrating__rate')))
 
         page = request.GET.get('page')
         paginator = Paginator(film_list, per_page=10)
@@ -91,17 +93,17 @@ def list(request):
 def detail(request, pk):
     film = get_object_or_404(Film, pk=pk)
     current_film = Film.objects.filter(id=film.id).annotate(average_score=Round(Avg('filmrating__rate')),
-                                                        votes=Count('filmrating__user', distinct=True), 
-                                                        inwatchlist=Count('filmwatchlist__user', distinct=True))
+                                                            votes=Count('filmrating__user', distinct=True),
+                                                            inwatchlist=Count('filmwatchlist__user', distinct=True))
     if request.user.is_authenticated():
         add_film_id = request.GET.get('add_film_id', None)
         del_film_id = request.GET.get('del_film_id', None)
         add_watchlist = request.GET.get('add_watchlist', None)
-        del_watchlist= request.GET.get('del_watchlist', None)
+        del_watchlist = request.GET.get('del_watchlist', None)
         rate = request.GET.get('rate', None)
         activ_user = get_object_or_404(User, username=request.user)
 
-        if request.method == 'GET': 
+        if request.method == 'GET':
             if add_film_id is not None and rate is not None:
                 if FilmRating.objects.filter(user=activ_user, film=film).exists():
                     FilmRating.objects.filter(user=activ_user, film=film).update(rate=rate)
@@ -138,5 +140,78 @@ def detail(request, pk):
 
 
 def top_rated(request):
+    if request.user.is_authenticated():
+        add_film_id = request.GET.get('add_film_id', None)
+        del_film_id = request.GET.get('del_film_id', None)
+        add_watchlist = request.GET.get('add_watchlist', None)
+        del_watchlist = request.GET.get('del_watchlist', None)
+        rate = request.GET.get('rate', None)
+        activ_user = get_object_or_404(User, username=request.user)
+        activ_profile = get_object_or_404(Profile, user=activ_user)
 
-    return render(request, 'film/top_rated.html')
+        if request.method == 'GET':
+            if add_film_id is not None and rate is not None:
+                film = get_object_or_404(Film, id=add_film_id)
+                if FilmRating.objects.filter(user=activ_user, film=film).exists():
+                    FilmRating.objects.filter(user=activ_user, film=film).update(rate=rate)
+                else:
+                    FilmRating.objects.create(user=activ_user, film=film, rate=rate)
+
+            if del_film_id is not None and rate is not None:
+                film = get_object_or_404(Film, id=del_film_id)
+                FilmRating.objects.filter(user=activ_user, film=film, rate=rate).delete()
+
+            if add_watchlist is not None:
+                film = get_object_or_404(Film, id=add_watchlist)
+                if FilmWatchlist.objects.filter(user=activ_user, film=film).exists():
+                    FilmWatchlist.objects.filter(user=activ_user, film=film).update()
+                else:
+                    FilmWatchlist.objects.create(user=activ_user, film=film)
+
+            if del_watchlist is not None:
+                film = get_object_or_404(Film, id=del_watchlist)
+                FilmWatchlist.objects.filter(user=activ_user, film=film).delete()
+
+        film_list = Film.objects.get_queryset().annotate(
+            average_score=Round(Avg('filmrating__rate'))).order_by('-average_score')[:100]
+
+        filmrating = FilmRating.objects.filter(user=activ_user)
+        f_id = filmrating.values_list('film__id', flat=True)
+
+        watchlist = FilmWatchlist.objects.filter(user=activ_user)
+        watchlist_id = watchlist.values_list('film__id', flat=True)
+
+        page = request.GET.get('page')
+        paginator = Paginator(film_list, per_page=10)
+        try:
+            films = paginator.page(page)
+        except PageNotAnInteger:
+            films = paginator.page(1)
+        except EmptyPage:
+            films = paginator(paginator.num_pages)
+
+        context = {
+            'f_id': f_id,
+            'films': films,
+            'filmrating': filmrating,
+            'activ_profile': activ_profile,
+            'watchlist_id': watchlist_id,
+        }
+        return render(request, 'film/top_rated.html', context)
+    else:
+        film_list = Film.objects.get_queryset().annotate(
+            average_score=Round(Avg('filmrating__rate'))).order_by('-average_score')[:100]
+
+        page = request.GET.get('page')
+        paginator = Paginator(film_list, per_page=10)
+        try:
+            films = paginator.page(page)
+        except PageNotAnInteger:
+            films = paginator.page(1)
+        except EmptyPage:
+            films = paginator(paginator.num_pages)
+
+        context = {
+            'films': films,
+        }
+        return render(request, 'film/top_rated.html', context)
--- serials/views.py
+++ serials/views.py
@@ -138,5 +138,78 @@ def detail(request, pk):
 
 
 def top_rated(request):
+    if request.user.is_authenticated():
+        add_serial_id = request.GET.get('add_serial_id', None)
+        del_serial_id = request.GET.get('del_serial_id', None)
+        add_watchlist = request.GET.get('add_watchlist', None)
+        del_watchlist = request.GET.get('del_watchlist', None)
+        rate = request.GET.get('rate', None)
+        activ_user = get_object_or_404(User, username=request.user)
+        activ_profile = get_object_or_404(Profile, user=activ_user)
 
-    return render(request, 'serial/top_rated.html')
+        if request.method == 'GET':
+            if add_serial_id is not None and rate is not None:
+                serial = get_object_or_404(Serial, id=add_serial_id)
+                if SerialRating.objects.filter(user=activ_user, serial=serial).exists():
+                    SerialRating.objects.filter(user=activ_user, serial=serial).update(rate=rate)
+                else:
+                    SerialRating.objects.create(user=activ_user, serial=serial, rate=rate)
+
+            if del_serial_id is not None and rate is not None:
+                serial = get_object_or_404(Serial, id=del_serial_id)
+                SerialRating.objects.filter(user=activ_user, serial=serial, rate=rate).delete()
+
+            if add_watchlist is not None:
+                serial = get_object_or_404(Serial, id=add_watchlist)
+                if SerialWatchlist.objects.filter(user=activ_user, serial=serial).exists():
+                    SerialWatchlist.objects.filter(user=activ_user, serial=serial).update()
+                else:
+                    SerialWatchlist.objects.create(user=activ_user, serial=serial)
+
+            if del_watchlist is not None:
+                serial = get_object_or_404(Serial, id=del_watchlist)
+                SerialWatchlist.objects.filter(user=activ_user, serial=serial).delete()
+
+        serial_list = Serial.objects.get_queryset().annotate(
+            average_score=Round(Avg('serialrating__rate'))).order_by('-average_score')[:100]
+
+        serialrating = SerialRating.objects.filter(user=activ_user)
+        s_id = serialrating.values_list('serial__id', flat=True)
+
+        watchlist = SerialWatchlist.objects.filter(user=activ_user)
+        watchlist_id = watchlist.values_list('serial__id', flat=True)
+
+        page = request.GET.get('page')
+        paginator = Paginator(serial_list, per_page=10)
+        try:
+            serials = paginator.page(page)
+        except PageNotAnInteger:
+            serials = paginator.page(1)
+        except EmptyPage:
+            serials = paginator(paginator.num_pages)
+
+        context = {
+            's_id': s_id,
+            'serials': serials,
+            'serialrating': serialrating,
+            'activ_profile': activ_profile,
+            'watchlist_id': watchlist_id,
+        }
+        return render(request, 'serial/top_rated.html', context)
+    else:
+        serial_list = Serial.objects.get_queryset().annotate(
+            average_score=Round(Avg('serialrating__rate'))).order_by('-average_score')[:100]
+
+        page = request.GET.get('page')
+        paginator = Paginator(serial_list, per_page=10)
+        try:
+            serials = paginator.page(page)
+        except PageNotAnInteger:
+            serials = paginator.page(1)
+        except EmptyPage:
+            serials = paginator(paginator.num_pages)
+
+        context = {
+            'serials': serials,
+        }
+        return render(request, 'serial/top_rated.html', context)
